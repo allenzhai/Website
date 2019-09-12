bec20d783125778f04d8172dbf1d82e3578b20ee
Allen Zhai <37985535+allenzhai@users.noreply.github.com>
2019-09-11 19:26:09 -0700

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ class Resume extends Component {
 	render() {
 		return (
 			<div className = "resumeContainer">
-				<iframe title ="Allen Zhai's Resume" src="https://drive.google.com/file/d/16pw58mm5yi-bKcJLgETh_WKD4u6OFG1t/preview" width="100%" height="100%"></iframe>
+				<iframe title ="Allen Zhai's Resume" src="https://drive.google.com/file/d/17sefbgErCqE6hx_e871QAWlZqduJ1_ZI/preview" width="100%" height="100%"></iframe>
 			</div>
 		);
 	}
