67c32f6c1f1b78ea144c653c92b53ef2de34edb9
allenzhai <azhai@calpoly.edu>
2020-01-04 17:38:39 -0800

    Updates

---
+++ precache-manifest.c58a87c2832600d493a3db73f396f07b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10709495b94afd11ab3c6a378744d761",
+    "url": "/,allenzhai.ml/index.html"
+  },
+  {
+    "revision": "039ee616ead193736a92",
+    "url": "/,allenzhai.ml/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "931ae6abbafc2f402ddc",
+    "url": "/,allenzhai.ml/static/css/main.35d4a129.chunk.css"
+  },
+  {
+    "revision": "039ee616ead193736a92",
+    "url": "/,allenzhai.ml/static/js/2.e03c9a0c.chunk.js"
+  },
+  {
+    "revision": "931ae6abbafc2f402ddc",
+    "url": "/,allenzhai.ml/static/js/main.4d9f54ca.chunk.js"
+  },
+  {
+    "revision": "dd29b0055f959cd5319c",
+    "url": "/,allenzhai.ml/static/js/runtime~main.11a5708a.js"
+  },
+  {
+    "revision": "e4634c74dd5df0a1820106a625608e40",
+    "url": "/,allenzhai.ml/static/media/203.e4634c74.png"
+  },
+  {
+    "revision": "7efe1aff2ca455a0788e74566cc489da",
+    "url": "/,allenzhai.ml/static/media/TTT.7efe1aff.png"
+  },
+  {
+    "revision": "1b044063502e14913fd63aebe10cdda7",
+    "url": "/,allenzhai.ml/static/media/candid.1b044063.jpg"
+  },
+  {
+    "revision": "0ad58e5f745f86253ab87e834ea2c4f4",
+    "url": "/,allenzhai.ml/static/media/emailicon.0ad58e5f.svg"
+  },
+  {
+    "revision": "b1e5a3e142adf129b6bdbcedaacc77ae",
+    "url": "/,allenzhai.ml/static/media/header_wallpaper.b1e5a3e1.jpg"
+  },
+  {
+    "revision": "df1ccb844acaa3e8b8cc812a0f1dfab5",
+    "url": "/,allenzhai.ml/static/media/healthBot.df1ccb84.png"
+  },
+  {
+    "revision": "94393125069eb17ba4eaf8b55133b7d8",
+    "url": "/,allenzhai.ml/static/media/linkedinicon.94393125.png"
+  },
+  {
+    "revision": "09c8bab3ee645c0944ddf637d6259038",
+    "url": "/,allenzhai.ml/static/media/remind.09c8bab3.png"
+  },
+  {
+    "revision": "353ccf1c4f429d4aa98c5dd359dee709",
+    "url": "/,allenzhai.ml/static/media/tools.353ccf1c.png"
+  },
+  {
+    "revision": "53bcffd2af2d7c88c43797ddca081e62",
+    "url": "/,allenzhai.ml/static/media/website.53bcffd2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dbb79ed6bbef222a21c8135f82ec4d4.js"
+  "/,allenzhai.ml/precache-manifest.c58a87c2832600d493a3db73f396f07b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/,allenzhai.ml/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
